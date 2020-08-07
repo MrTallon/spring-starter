82d7425a5d20dd035198efbd9f4b8c36cf1db7dd
tallon <352420160@qq.com>
2020-08-08 01:05:28 +0800

    Init commit

---
+++ repository/repository-core/src/test/java/com/tallon/repository/core/mapper/CoreAdminMapperTest.java
@@ -0,0 +1,34 @@
+package com.tallon.repository.core.mapper;
+
+import com.tallon.repository.core.CoreRepositoryApplication;
+import com.tallon.repository.core.domain.CoreAdmin;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CoreRepositoryApplication.class)
+public class CoreAdminMapperTest {
+
+    @Resource
+    private CoreAdminMapper mapper;
+
+    @Test
+    public void test() {
+        System.out.println("Hello World");
+    }
+
+    @Test
+    public void get1() {
+        List<CoreAdmin> coreAdmins = mapper.selectList(null);
+        coreAdmins.forEach(System.out::print);
+        Assert.assertNotNull(coreAdmins);
+    }
+
+}
