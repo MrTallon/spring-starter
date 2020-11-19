3c55e398e64781a88ebdf88603c681b73319bc30
tallon <352420160@qq.com>
2020-11-19 18:04:17 +0800

    权限表

---
+++ repository/repository-core/src/main/java/com/tallon/repository/core/domain/TbPermission.java
@@ -0,0 +1,48 @@
+package com.tallon.repository.core.domain;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.tallon.common.base.BaseDomain;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 权限表
+ * </p>
+ *
+ * @author Tallon
+ * @since 2020-11-19
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@TableName("tb_permission")
+public class TbPermission extends BaseDomain {
+
+    private static final long serialVersionUID = 1889589835446748446L;
+    /**
+     * 父权限
+     */
+    private Long parentId;
+
+    /**
+     * 权限名称
+     */
+    private String name;
+
+    /**
+     * 权限英文名称
+     */
+    private String enname;
+
+    /**
+     * 授权路径
+     */
+    private String url;
+
+    /**
+     * 备注
+     */
+    private String description;
+}
---
+++ repository/repository-core/src/main/java/com/tallon/repository/core/mapper/TbPermissionMapper.java
@@ -0,0 +1,24 @@
+package com.tallon.repository.core.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tallon.repository.core.domain.TbPermission;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 权限表 Mapper 接口
+ * </p>
+ *
+ * @author Tallon
+ * @since 2020-11-19
+ */
+public interface TbPermissionMapper extends BaseMapper<TbPermission> {
+    /**
+     * 查询用户权限名称
+     * @param id 用户id
+     * @return 权限名称列表
+     */
+    List<String> tbPermissions(@Param("id") Long id);
+}
